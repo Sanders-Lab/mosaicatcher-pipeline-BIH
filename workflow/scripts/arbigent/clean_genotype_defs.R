d7dc10db0d8a68009f31185df82280c94b0d590d
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2024-05-10 12:47:19 +0200

    make_output_vcf rule works with changes
    checked for filtering empty df in add_info to tab file. They were
    filtering and adding a value to a col (GT). But the filtering was
    resulting in an empty df, which led to the error "replacement has 1 row,
    data has 0". To circumvent, I added a check for nrow(subset) > 0. Now
    works like a charm. The logic does not change.

--- workflow/scripts/arbigent/clean_genotype_defs.R
+++ workflow/scripts/arbigent/clean_genotype_defs.R
@@ -28,8 +28,12 @@ add_gts_revisited <- function(tab, bias_factor, bias_add_factor, cutoff) {
   tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$GT <-
     tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_hard
 
-  tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GT <-
-    tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard # removed lowconf here.
+  subset <- tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]
+  if (nrow(subset) > 0) {
+      
+      tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GT <-
+        tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard # removed lowconf here.
+  }
 
   tab[(tab$confidence_hard_over_second == 0) & (tab$confidence_nobias_over_hard == 0), "GT"] <- "noreads"
   return(tab)
@@ -43,8 +47,12 @@ add_gts_revisited_lowconf <- function(tab, bias_factor, bias_add_factor, cutoff)
 
   tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$GT <-
     tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_hard
-  tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GT <-
-    paste0(tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard, "_lowconf") # removed lowconf here.
+
+  subset <- tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]
+  if (nrow(subset) > 0 ) {
+    tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GT <- 
+        paste0(tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard, "_lowconf") # removed lowconf here.
+  }
 
   tab[(tab$confidence_hard_over_second == 0) & (tab$confidence_nobias_over_hard == 0), "GT"] <- "noreads"
   return(tab)
@@ -100,44 +108,56 @@ add_long_gts_revisited <- function(tab, bias_factor, bias_add_factor, cutoff) {
   tab$confidence_nobias_over_hard <- round(tab$confidence_nobias_over_hard, 1)
 
 
-  tab[tab$confidence_nobias_over_hard >= bias_factor, ]$GTL <-
-    paste(tab[tab$confidence_nobias_over_hard >= bias_factor, ]$pred_nobias,
-      "T",
-      tab[tab$confidence_nobias_over_hard >= bias_factor, ]$pred_hard,
-      tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_hard_over_second + tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_nobias_over_hard,
-      tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_hard_over_second,
-      sep = ":"
-    )
+  subset <- tab[tab$confidence_nobias_over_hard >= bias_factor, ]
+  if (nrow(subset) > 0) {
+      tab[tab$confidence_nobias_over_hard >= bias_factor, ]$GTL <-
+          paste(tab[tab$confidence_nobias_over_hard >= bias_factor, ]$pred_nobias,
+                "T",
+                tab[tab$confidence_nobias_over_hard >= bias_factor, ]$pred_hard,
+                tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_hard_over_second + tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_nobias_over_hard,
+                tab[tab$confidence_nobias_over_hard >= bias_factor, ]$confidence_hard_over_second,
+                sep = ":"
+          )
+  }
 
-  tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$GTL <-
-    paste(paste0(tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$pred_nobias),
-      "T",
-      tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard,
-      tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second +
-        tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_nobias_over_hard,
-      tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second,
-      sep = ":"
-    )
+  subset <- tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]
+  if (nrow(subset) > 0) {
+      tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$GTL <-
+          paste(paste0(tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$pred_nobias),
+                "T",
+                tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard,
+                tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second +
+                    tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_nobias_over_hard,
+                tab[(tab$confidence_nobias_over_hard >= bias_factor) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second,
+                sep = ":"
+          )
+  }
 
-  tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$GTL <-
-    paste(tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_hard,
-      "T",
-      tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_nobias,
-      tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_hard_over_second,
-      tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_hard_over_second +
-        tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_nobias_over_hard,
-      sep = ":"
-    )
+  subset <- tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]
+  if (nrow(subset) > 0) {
+      tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$GTL <-
+          paste(tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_hard,
+                "T",
+                tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$pred_nobias,
+                tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_hard_over_second,
+                tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_hard_over_second +
+                    tab[(tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor), ]$confidence_nobias_over_hard,
+                sep = ":"
+          )
+  }
 
-  tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GTL <-
-    paste(tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard,
-      "F",
-      tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_nobias,
-      tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second,
-      tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second +
-        tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_nobias_over_hard,
-      sep = ":"
-    )
+  subset <- tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]
+  if (nrow(subset) > 0) {
+      tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$GTL <-
+          paste(tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_hard,
+                "F",
+                tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$pred_nobias,
+                tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second,
+                tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_hard_over_second +
+                    tab[((tab$confidence_nobias_over_hard < bias_factor) | (tab$confidence_nobias_over_hard < bias_add_factor)) & (tab$confidence_hard_over_second < cutoff), ]$confidence_nobias_over_hard,
+                sep = ":"
+          )
+  }
 
 
   tab[(tab$confidence_hard_over_second == 0) & (tab$confidence_nobias_over_hard == 0), "GTL"] <- "noreads"
